a84d8183c3bf02a50f4f5653ae13f43d97412819
kizaleks <115622709+kizaleks@users.noreply.github.com>
2023-11-22 08:22:50 +0300

    Update urls.py

--- mysilant/config/urls.py
+++ mysilant/config/urls.py
@@ -18,27 +18,13 @@ from rest_framework import permissions
 from rest_framework.authtoken import views
 from django.contrib import admin
 from django.urls import path, include, re_path
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
 from django.conf import settings
 from django.conf.urls.static import static
 
-schema_view = get_schema_view(
-   openapi.Info(
-      title="MySilant API",
-      default_version='v1',
-      description="Test description",
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
-)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("account/", include("account.urls")),
     path("mysilant/", include("mysilant.urls")),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('json/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-
+    
 ]
